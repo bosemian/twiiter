48e46f02f6ce64f3bce27e9c80ce1ed018becfeb
spby <siwanon.turbow@gmail.com>
2017-05-14 20:33:19 +0700

    initial firebase vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
+import router from './router'
+import firebase from 'firebase'
+
 import App from './App'
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyBF4WOu-T7_3_2-ucYXJBJEGXGvWK2c6UI',
+  authDomain: 'twiiter-b335f.firebaseapp.com',
+  databaseURL: 'https://twiiter-b335f.firebaseio.com',
+  projectId: 'twiiter-b335f',
+  storageBucket: 'twiiter-b335f.appspot.com',
+  messagingSenderId: '567003771596'
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  ...App
 })
