8f0779dfd618e227e0bbdb251e1ee02424ef059e
spby <siwanon.turbow@gmail.com>
2017-05-16 10:07:05 +0700

    create Form components

---
+++ src/components/ProfileEdit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ui segment">
+    <h3 class="ui header">Edit Profile</h3>
+    <profile-form @save="saveForm"></profile-form>
+  </div>
+</template>
+
+<script>
+import ProfileForm from './ProfileForm'
+export default {
+  components: {
+    ProfileForm
+  },
+  methods: {
+    saveForm () {
+      console.log('save from profile edit')
+    }
+  }
+}
+</script>
---
+++ src/components/ProfileForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="ui form" @submit.prevent="save">
+    <div class="field">
+      <label>Name</label>
+      <input v-model="name">
+    </div>
+    <div class="field">
+      <label>Description</label>
+      <input v-model="description">
+    </div>
+    <button class="ui submit button blue">Save</button>
+    <div class="ui button red">Cancel</div>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: '',
+    description: ''
+  }),
+  methods: {
+    save () {
+      this.$emit('save')
+    }
+  }
+}
+</script>
